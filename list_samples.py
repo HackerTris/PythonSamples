885eb527eee04fee037d52357d4e5f2610f40802
Robann Mateja <robannmateja@gmail.com>
2013-05-13 16:43:05 -0700

    Update with some more list functions

--- list_samples.py
+++ list_samples.py
@@ -28,7 +28,52 @@ def list_swap_min_max (a_list):
 
     return a_list
 
+def unique_chars(test_string):
+    """ Test if a string has unique chars.
+    Case sensitive; l<> L
 
+    The algorithm we will use is to take the first char, and then look
+    for occurences in the next slice of the string.  We will keep doing this until
+    the string is exhausted or we found a match, indicating that the string does
+    not consist of unique chars.
+
+    Blanks are not counted! 
+    """
+    test_string="".join(test_string.split())  #squeeze out the white space
+    unique=True  # assume true to start with...
+    test_char=test_string[0]
+    test_slice=test_string[1:]
+
+
+    while len(test_slice) > 0:
+        if test_slice.find(test_char)>=0:
+            unique=False
+            break
+        test_char=test_slice[0]
+        test_slice=test_slice[1:]
+    
+    return unique
+    
+
+def reverse_string(test_string):
+    """Reverse a string. Will do this two different ways; the easy python way
+    and the brute force way.
+
+    """
+
+    #easy python way
+
+    #rev_string=test_string[::-1]
+
+    #brute force way
+    rev_string=""
+
+    print("The length is: ",len(test_string))
+ 
+    for i in reversed(range(0,len(test_string))):
+        rev_string=rev_string+test_string[i]    
+        
+    return rev_string
 
 
     
--- test_list_samples.py
+++ test_list_samples.py
@@ -16,6 +16,26 @@ class ListSampleTest(unittest.TestCase):
         expected_list=[99,-30,8,99,6,-30,65]
         actual_list=list_swap_min_max(test_list)
         self.assertEqual(expected_list,actual_list,"Lists did not match")
+
+    def test_unique_chars(self):
+        """ Test algorithm for a string that has all unique chars
+        """
+        test_string1="I have several repeated chars."
+        test_string2="I am uniqe. "
+
+        # requirements:  all unique characters.  Case sensitive! so I <> i.
+        # Blanks and white space are not counted!
+
+        self.assertEqual(unique_chars(test_string1),False)
+        self.assertEqual(unique_chars(test_string2),True)
+        self.assertEqual(unique_chars("aa"),False)
+
+    def test_reverse_string(self):
+        
+        test_string="Reverse Me"
+        expected_string="eM esreveR"
+        self.assertEqual(reverse_string(test_string),expected_string)
+        
         
 if __name__== "__main__" :
     try:
