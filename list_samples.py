67252d055de319f93990b303d5418e01885a4517
Robann Mateja <robannmateja@gmail.com>
2013-05-17 15:26:52 -0700

    Completed matrix example and test

--- list_samples.py
+++ list_samples.py
@@ -1,6 +1,16 @@
-""" Contains code for Python list sample snippets.
+""" Contains code for Python list sample programs.
+
+What is this?  Well, I worked through many of the coding problems in "Cracking he
+Coding Interview", by Gayle McDowell, except I did these programming problems
+in Python, rather than C++ or Java.
+
+Each category of samples also has a unit test program to go along with it.  I
+used test driven development to develop the algorithms in each of these sample
+programs.
 
 """
+
+import copy  # used to make deep copies of lists
 
 def list_swap_min_max (a_list):
     """ Swap min and max values in the list.
@@ -17,8 +27,6 @@ def list_swap_min_max (a_list):
 
     max_indexes=[i for i,x in enumerate(a_list) if x == max_val]
     
-    print("The min indexes are: ",min_indexes)
-    print("The max indexes are: ",max_indexes)
 
     for i in range(len(min_indexes)):
         a_list[min_indexes[i]]=max_val
@@ -68,7 +76,6 @@ def reverse_string(test_string):
     #brute force way
     rev_string=""
 
-    print("The length is: ",len(test_string))
  
     for i in reversed(range(0,len(test_string))):
         rev_string=rev_string+test_string[i]    
@@ -76,8 +83,52 @@ def reverse_string(test_string):
     return rev_string
 
 
-    
+def permutation_string(string1,string2):
+    """ ReturnsTrue if the strings are permutations of eachother
 
+    """
+
+    s1="".join(sorted(string1))
+    s2="".join(sorted(string2))
+    return (s1==s2)
+
+
+def matrix_transform(matrix):
+    """
+
+    some notes
+    determine num rows and columns
+    for each row, look for 0's.  Use index to find the first 0.  Then slice the
+    list and look for next 0.  So if the row is [1,0,2], first 0 index is at 1. Then
+    slice the list and look for more:  row[2:]
+
+    so, for each row, look for all 0's in the row.  Then remeber the position, and for each row
+    afer the current row, 0 out in the column postion.   Always build a new matrix because
+    you don't want to work on a partially transformed one.
     
+    """
+    
+    num_rows=len(matrix)
+    
+    #each of the internal lists must consist of the same
+    #number of elements... but not checking this for now
+    
+    num_cols=len(matrix[0])
+
+    #initialize the transformed matrix to be a copy of the matrix.
+    #A deep copy is needed, rather than just initializing another variable.
+
+    transformed_matrix=copy.deepcopy(matrix)
+    
+    for row in range(0,num_rows):       
+        for column in range(0,num_cols):
+            if matrix[row][column]==0:              
+                transformed_matrix[row]=[0,0,0]
+                for r in range(row+1,num_rows):
+                    transformed_matrix[r][column]=0
+                for r in range(0,row):
+                    transformed_matrix[r][column]=0
+         
+    return transformed_matrix
 
     
--- test_list_samples.py
+++ test_list_samples.py
@@ -12,7 +12,6 @@ class ListSampleTest(unittest.TestCase):
         """
         
         test_list =[-30,99,8,-30,6,99,65]
-        print("The min value is: ",min(test_list))
         expected_list=[99,-30,8,99,6,-30,65]
         actual_list=list_swap_min_max(test_list)
         self.assertEqual(expected_list,actual_list,"Lists did not match")
@@ -31,11 +30,60 @@ class ListSampleTest(unittest.TestCase):
         self.assertEqual(unique_chars("aa"),False)
 
     def test_reverse_string(self):
-        
+        """ Test string reverse operation.  easy to do in python with backward
+        skips s[::-1]
+        """
         test_string="Reverse Me"
         expected_string="eM esreveR"
         self.assertEqual(reverse_string(test_string),expected_string)
+
+    def test_string_permutation(self):
+        """ Test function which determines if one string is a permutation of
+        another.
+        """
+        test_string1="cba"
+        test_string2="acb"
+
+        self.assertEqual(permutation_string(test_string1,test_string2),True)
+
+        # now test the reverse
+
+        test_string3="da"
+        self.assertEqual(permutation_string(test_string1,test_string3),False)
+
+    def test_matrix_transform(self):
+        """ Test function that, given an mxn matrix, if any element is 0, then
+        all elements in the row are tranformed to 0 and all elements in the column.
+
+        Examples:  assume the matrix is implemented as a list of lists.
+
+        [[0,1,2], [3,4,5]] represents a 2x3 matrix, where there are two rows
+        and three columns.  Since element 0,0 is 0, then all elements in the first row
+        are 0 and all elements in the first column are zero.  This would
+        lead to a matrix represented as: [[0,0,0], [0,4,5]]
         
+        """
+
+        m_test=[[0,1,2],[3,4,5]]
+        m_expected=[[0,0,0],[0,4,5]]
+        self.assertEqual(matrix_transform(m_test),m_expected)
+
+        #test the case where there are no 0's, so the transformed matrix is the
+        #same as the starting matrix
+
+        m_test=[[1,2,3],[4,5,6]]
+        self.assertEqual(matrix_transform(m_test),m_test)
+
+        #test case with 0's in different positions in each row
+
+        m_test=[[0,1,2],[3,0,4],[5,6,0]]
+        m_expected=[[0,0,0],[0,0,0],[0,0,0]]
+
+        self.assertEqual(matrix_transform(m_test),m_expected)
+
+        m_test=[[0,0,1],[2,3,4],[5,6,7]]
+        m_expected=[[0,0,0],[0,0,4],[0,0,7]]
+        self.assertEqual(matrix_transform(m_test),m_expected)
         
 if __name__== "__main__" :
     try:
